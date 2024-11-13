9cd7780f0abf0248931ed378206bf91b6e7d273a
pablovirtual <143946760+pablovirtual@users.noreply.github.com>
2024-11-12 23:23:08 -0600

    Se creo la portada , el login ,se realizo la conexion con la base de datos, se verifico que todos los archivos esten vinculados de manera correcta

---
+++ data_base.php
@@ -0,0 +1,17 @@
+<?php
+// Datos de la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "Ufax1824";
+$dbname = "bdvh";
+// Creando la conexxion con la base de datos
+$conn = new mysqli($servername, $username, $password, $dbname);
+// revisar la conexion
+if ($conn->connect_error) {
+    // si hay un error en la conexion, se muestra un mensaje de error
+die("Connection failed: " . $conn->connect_error);
+}
+// si la conexion es exitosa, se muestra un mensaje de exito
+echo "Connected successfully";
+
+?>
