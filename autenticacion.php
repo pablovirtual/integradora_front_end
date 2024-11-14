65d8e8df4ce15614db6b27ade210e3e09db045eb
pablovirtual <143946760+pablovirtual@users.noreply.github.com>
2024-11-13 23:28:45 -0600

    se agrego la libreria de jquery y jquery ui

--- autenticacion.php
+++ autenticacion.php
@@ -1,3 +1,18 @@
-<?php
+<?php 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--aqui se debe de hacer la autenticacion del usuario-->
+
+    <script src="/jquery-3.7.1.min.js"></script>
+    <script src="/script.js"></script>    
+</body>
+</html>
--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-
+// aquie se realiza la conexion de la base de datos 
+$(document).ready(function(){
+$.ajax({
+    URL:"data_base.php",
+    
+})
+});
 
